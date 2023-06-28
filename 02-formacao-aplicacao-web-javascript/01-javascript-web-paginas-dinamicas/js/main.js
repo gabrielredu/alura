76177bfad5ee5cfcd43d2b80f774c3348caf5939
Gabriel Redu <gabredu@publicisgroupe.net>
2023-06-28 15:46:58 -0300

    Finalizado o curso

--- 02-formacao-aplicacao-web-javascript/01-javascript-web-paginas-dinamicas/js/main.js
+++ 02-formacao-aplicacao-web-javascript/01-javascript-web-paginas-dinamicas/js/main.js
@@ -2,7 +2,16 @@ const listBtn = document.querySelectorAll('.tecla');
 const listAudio = document.querySelectorAll('audio');
 
 listBtn.forEach((tecla, i) => {
-    tecla.addEventListener('click', () => {
-        listAudio[i].play();
+    tecla.addEventListener('keydown', (event) => {
+        if (event.key == 'Enter' || event.key == ' ') {
+            listAudio[i].play()
+            tecla.classList.add('ativa');
+        }
+    })
+
+    tecla.addEventListener('keyup', (event) => {
+        if (event.key == 'Enter' || event.key == ' ') {
+            tecla.classList.remove('ativa');
+        }
     })
 })
