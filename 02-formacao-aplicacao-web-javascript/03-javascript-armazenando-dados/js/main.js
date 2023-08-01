9cf639a2fc208d55acb528af4d98f8f2c77be505
Gabriel Redu <gabredu@publicisgroupe.net>
2023-07-31 23:16:15 -0300

    inserindo dados no local storage

--- 02-formacao-aplicacao-web-javascript/03-javascript-armazenando-dados/js/main.js
+++ 02-formacao-aplicacao-web-javascript/03-javascript-armazenando-dados/js/main.js
@@ -1,13 +1,25 @@
 const form = document.querySelector('#novoItem');
+const lista = document.querySelector('#lista');
+
+function criarElemento (nome, quantidade) {
+    const novoItem = document.createElement('li');
+    const quantidadeItem = document.createElement('strong');
+
+    novoItem.classList.add('item');
+    quantidadeItem.innerHTML = quantidade;
+    novoItem.appendChild(quantidadeItem);
+    novoItem.innerHTML += nome;
+
+    lista.appendChild(novoItem);
+}
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    const nome = e.target.elements['nome'].value;
-    const quantidade = e.target.elements['quantidade'].value;
+    const nome = e.target.elements['nome'];
+    const quantidade = e.target.elements['quantidade'];
 
-    criarElemento(nome, quantidade);
+    criarElemento(nome.value, quantidade.value);
+
+    nome.value = '';
+    quantidade.value = '';
 })
-
-function criarElemento (nome, quantidade) {
-    
-}
