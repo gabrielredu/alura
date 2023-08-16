e52042db1a21f3bd9087e5ffc32c0457a5f76f2f
Gabriel <gabrielredu@gmail.com>
2023-08-16 09:25:56 -0300

    validacao feita

--- 02-formacao-aplicacao-web-javascript/04-validacao-reconhecimento-voz/app/validacao.js
+++ 02-formacao-aplicacao-web-javascript/04-validacao-reconhecimento-voz/app/validacao.js
@@ -1,15 +1,34 @@
 function validacao(chute) {
     const numero = parseInt(chute);
 
-    if (Number.isNaN(numero)) {
-        console.log('Não é um número');
+    if (isNotANumber(numero)) {
+        chuteElements.innerHTML += `<div class="erro">Você precisa falar somente números!</div>`;
     }
 
     if (numberInsideRange(numero)) {
-        console.log('Número fora do alcance')
+        chuteElements.innerHTML += `<div class="alerta">Você precisa falar números entre ${menorValor} e ${maiorValor}</div>`;
+    }
+
+    if (numero === numeroGerado) {
+        document.body.innerHTML = `
+            <h1>Parabéns!</h1>
+            <h3>Você acertou! O número secreto era ${numeroGerado}!<br></h3>
+            <h3>Deseja jogar outra vez?</h3>
+            <button class="start-game">Jogar outra vez</button>
+        `
+        
+        const buttonRestart = document.querySelector('.start-game');
+
+        buttonRestart.addEventListener('click', () => {
+            document.location.reload();
+        })
     }
 
     function numberInsideRange (chute) {
         return chute < menorValor  || chute > maiorValor;
+    }
+
+    function isNotANumber (chute) {
+        return Number.isNaN(chute);
     }
 }
