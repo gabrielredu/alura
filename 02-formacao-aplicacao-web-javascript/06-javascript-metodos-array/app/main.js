b9a68f7af52a378ddc16853714135010bdbd2004
Gabriel <gabrielredu@gmail.com>
2023-08-22 00:59:08 -0300

    curso finalizado mas array.reduce nao implementado

--- 02-formacao-aplicacao-web-javascript/06-javascript-metodos-array/app/main.js
+++ 02-formacao-aplicacao-web-javascript/06-javascript-metodos-array/app/main.js
@@ -2,6 +2,11 @@ let livros = [];
 const livrosDiv = document.querySelector('#livros');
 const endpointAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
 const botoes = document.querySelectorAll('.btn');
+const btnOrdenar = document.querySelector('#btnOrdenarPorPreco');
+const btnDisponiveis = document.querySelector('#btnLivrosDisponiveis');
+const valorTotalSection = document.querySelector('#valor_total_livros_disponiveis');
+
+valorTotalSection.innerHTML = '';
 
 botoes.forEach((btn) => {
     btn.addEventListener('click', () => {
@@ -40,11 +45,13 @@ function aplicarDesconto(livros) {
 
 // foreach function
 function exibirLivros (lista) {
+    valorTotalSection.innerHTML = '';
     livrosDiv.innerHTML = '';
     lista.forEach((livro) => {
+        let disponibilidade = livro.quantidade > 0 ? 'livro__imagens' : 'livro__imagens indisponivel';
         livrosDiv.innerHTML += `
         <div class="livro">
-        <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+        <img class="${disponibilidade}" src="${livro.imagem}" alt="${livro.alt}" />
         <h2 class="livro__titulo">${livro.titulo}</h2>
         <p class="livro__descricao">${livro.autor}</p>
         <p class="livro__preco" id="preco">R$${livro.preco.toFixed(2)}</p>
@@ -56,11 +63,46 @@ function exibirLivros (lista) {
     })
 }
 
+// filter function
 function filtrarLivros (categoria) {
     let livrosFiltro = livros.filter((livro) => {
         return livro.categoria == categoria;
     })
     return livrosFiltro;
 }
+
+// sort function
+btnOrdenar.addEventListener('click', () => {
+    let livrosOrdenados = livros.sort((a, b) => {
+        return a.preco - b.preco
+    })
+    exibirLivros(livrosOrdenados);
+})
+
+function livrosDisponiveis() {
+    let livrosDisponiveis = livros.filter((livro) => {
+        return livro.quantidade > 0
+    })
+    return livrosDisponiveis;
+}
+
+function valorTotalLivros(livros) {
+    let valorTotal = livros.reduce((acc, livro) => {
+        return acc + livro.preco;
+    })
+    console.log(valorTotal)
+    return valorTotal;
+}
+
+btnDisponiveis.addEventListener('click', () => {
+    let disponiveis = livrosDisponiveis();
+    let valorTotalFunc = valorTotalLivros(livros);
+    exibirLivros(disponiveis);
+    valorTotalSection.innerHTML = `
+        <div class="livros__disponiveis">
+            <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotalFunc}</span></p>
+        </div>
+    `
+})
 
 buscarLivrosAPI();
