a6054b8fc94a2b1356b3f69482d46a3dccda0b94
Gabriel Redu <gabredu@publicisgroupe.net>
2023-09-13 19:57:49 -0300

    Aula 3 exercicio 5

--- 02-formacao-aplicacao-web-javascript/08-javascript-validando-formularios/js/script.js
+++ 02-formacao-aplicacao-web-javascript/08-javascript-validando-formularios/js/script.js
@@ -1,10 +1,14 @@
+import ehUmCpf from "./valida-cpf.js";
+
 const camposFormulario = document.querySelectorAll('[required]');
 camposFormulario.forEach((campo) => {
-    campo.addEventListener('blur',() => {
+    campo.addEventListener('blur', () => {
         validarCampo(campo);
     })
 })
 
 function validarCampo(campo) {
-
+    if (campo.name == "cpf" && campo.value.length >= 11) {
+        ehUmCpf(campo);
+    }
 }
