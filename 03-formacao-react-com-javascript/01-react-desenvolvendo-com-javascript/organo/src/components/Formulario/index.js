5acdcd7aff465b3b0cd593c250fe2830288421b0
Gabriel <gabrielredu@gmail.com>
2023-10-13 01:19:26 -0300

    Aula 3 exercicio 2

--- 03-formacao-react-com-javascript/01-react-desenvolvendo-com-javascript/organo/src/components/Formulario/index.js
+++ 03-formacao-react-com-javascript/01-react-desenvolvendo-com-javascript/organo/src/components/Formulario/index.js
@@ -1,8 +1,14 @@
+import Botao from '../Botao';
 import CampoTexto from '../CampoTexto';
 import ListaSuspensa from '../ListaSuspensa';
 import './Formulario.css';
 
 const Formulario = () => {
+
+    function onSubmit (e) {
+        console.log('tá funcionando?')
+        e.preventDefault();
+    }
 
     const times = [
         'Programação', 'Front-End', 'Data Science', 'DevOps', 'UX e Design', 'Mobile', 'Inovação e Gestão'
@@ -10,12 +16,13 @@ const Formulario = () => {
 
     return (
         <section className='formulario'>
-            <form>
+            <form onSubmit={onSubmit}>
                 <h2>Preencha os campos para criar o card do colaborador</h2>
-                <CampoTexto label='Nome' placeholder='Digite seu nome' />
-                <CampoTexto label='Cargo' placeholder='Digite seu cargo' />
+                <CampoTexto required={true} label='Nome' placeholder='Digite seu nome' />
+                <CampoTexto required={true} label='Cargo' placeholder='Digite seu cargo' />
                 <CampoTexto label='Imagem' placeholder='Informe o endereço da imagem' />
-                <ListaSuspensa label='Time' itens={times} />
+                <ListaSuspensa required={true} label='Time' itens={times} />
+                <Botao>Criar Card</Botao>
             </form>
         </section>
     )
