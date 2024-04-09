04b9e5abbf010514cfdd696f49f79b91e262bdc9
Gabriel <gabrielredu@gmail.com>
2024-04-08 23:52:27 -0300

    Feature de excluir colaborador

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,15 +1,18 @@
 import './colaborador.css'
 
-const Colaborador = ({ colaborador, corDeFundo }) => {
-    return (<div className="colaborador">
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
+    return (
+    <div className="colaborador">
         <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
             <img src={colaborador.imagem} alt={colaborador.nome} />
         </div>
         <div className="rodape">
             <h4>{colaborador.nome}</h4>
             <h5>{colaborador.cargo}</h5>
+            <button className='deletar' onClick={aoDeletar}>Remover colaborador</button>
         </div>
-    </div>)
+    </div>
+    )
 }
 
 export default Colaborador
